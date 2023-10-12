b79c39700b986a2c5094f8432876ae9b2655cd35
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-10-12 12:59:55 +0330

    Add retrieve algorithm based on carto graphy to PCR algorithm

--- agents/pcr.py
+++ agents/pcr.py
@@ -1,11 +1,14 @@
 import torch
+from torch import softmax
 from torch.utils import data
-from utils.buffer.buffer import Buffer
+
 from agents.base import ContinualLearner
 from continuum.data_utils import dataset_transform
+from utils.buffer.buffer import Buffer
+from utils.buffer.carto_update import Carto_update
+from utils.loss import SupConLoss
 from utils.setup_elements import transforms_match, transforms_aug
 from utils.utils import maybe_cuda
-from utils.loss import SupConLoss
 
 
 class ProxyContrastiveReplay(ContinualLearner):
@@ -19,6 +22,7 @@ class ProxyContrastiveReplay(ContinualLearner):
         Online Setting (OnlineCLScenario). However, it
         can also be used in non-online scenarios
         """
+
     def __init__(self, model, opt, params):
         super(ProxyContrastiveReplay, self).__init__(model, opt, params)
         self.buffer = Buffer(model, params)
@@ -46,10 +50,9 @@ class ProxyContrastiveReplay(ContinualLearner):
                 batch_x_combine = torch.cat((batch_x, batch_x_aug))
                 batch_y_combine = torch.cat((batch_y, batch_y))
                 for j in range(self.mem_iters):
-                    logits, feas= self.model.pcrForward(batch_x_combine)
-                    novel_loss = 0*self.criterion(logits, batch_y_combine)
+                    logits, feas = self.model.pcrForward(batch_x_combine)
+                    novel_loss = 0 * self.criterion(logits, batch_y_combine)
                     self.opt.zero_grad()
-
 
                     mem_x, mem_y = self.buffer.retrieve(x=batch_x, y=batch_y)
                     if mem_x.size(0) > 0:
@@ -62,8 +65,7 @@ class ProxyContrastiveReplay(ContinualLearner):
                         mem_x_combine = torch.cat([mem_x, mem_x_aug])
                         mem_y_combine = torch.cat([mem_y, mem_y])
 
-
-                        mem_logits, mem_fea= self.model.pcrForward(mem_x_combine)
+                        mem_logits, mem_fea = self.model.pcrForward(mem_x_combine)
 
                         combined_feas = torch.cat([mem_fea, feas])
                         combined_labels = torch.cat((mem_y_combine, batch_y_combine))
@@ -81,10 +83,53 @@ class ProxyContrastiveReplay(ContinualLearner):
                         PSC = SupConLoss(temperature=0.09, contrast_mode='proxy')
                         novel_loss += PSC(features=cos_features, labels=combined_labels)
 
-
                     novel_loss.backward()
                     self.opt.step()
                 # update mem
-                self.buffer.update(batch_x, batch_y)
+                if self.buffer.update_method.__class__ is not Carto_update:
+                    self.buffer.update(batch_x, batch_y)
+
+            if self.buffer.update_method.__class__ is Carto_update:
+                self.model = self.model.eval()
+                carto_data_loader = data.DataLoader(train_dataset, batch_size=self.batch, shuffle=False)
+                last_index = 0
+                for batch_data in carto_data_loader:
+                    batch_x, batch_y = batch_data
+                    batch_x = maybe_cuda(batch_x, self.cuda)
+                    batch_y = maybe_cuda(batch_y, self.cuda)
+                    with torch.no_grad():
+                        features = self.model.forward(batch_x).unsqueeze(1)
+
+                    true_label_probs = softmax(features, dim=2).squeeze(1).gather(1, batch_y.unsqueeze(1)).squeeze(1)
+                    for sample_idx, prob in enumerate(true_label_probs):
+                        sample_data = self.carto_data[self.task_seen][last_index + sample_idx]
+                        sample_data.probabilities.append(prob.item())
+                        sample_data.index = last_index + sample_idx
+                        sample_data.experience = self.task_seen
+                        sample_data.label = batch_y[sample_idx].item()
+                    last_index += len(true_label_probs)
+                self.model = self.model.train()
+
+        if self.buffer.update_method.__class__ is Carto_update:
+            # update mem
+            carto_data_loader = data.DataLoader(train_dataset, batch_size=self.batch, shuffle=False)
+            x = None
+            y = None
+            for batch_data in carto_data_loader:
+                batch_x, batch_y = batch_data
+                if x is None:
+                    x = batch_x
+                    y = batch_y
+                else:
+                    x = torch.cat((x, batch_x))
+                    y = torch.cat((y, batch_y))
+
+            self.buffer.update(
+                x,
+                y,
+                task_seen=self.task_seen,
+                carto_data=self.carto_data,
+                classes_seen=self.classes_seen
+            )
 
         self.after_train()
